dcb81aa880b0bf9778bdb168adb0aae3c1cf0be8
UEG GAME DESIGN <ueggamedesign@UEGs-iMac.local>
2022-09-28 19:10:08 -0300

    Alterações para voo de teste

--- tirarFotoMuitoFoda.py
+++ tirarFotoMuitoFoda.py
@@ -8,13 +8,9 @@ me.connect()
 print("Bateria: ", me.get_battery())
 
 me.takeoff()
-sleep(20)
 me.move_up(50)
-sleep(20)
 me.move_forward(70)
-sleep(20)
-me.send_rc_control(0, 0, 0, 30)
-sleep(20)
+me.flip_back()
 
 #tirar foto
 #me.streamon()
@@ -23,5 +19,4 @@ sleep(20)
 #sleep(10)
 
 me.move_forward(70)
-sleep(20)
 me.land()
