3a89293e2456d13b4bbc29ae0a9f6f26a7e8577e
João Augusto Moreira Ananias <joao.ananias@aluno.ueg.br>
2022-10-03 18:29:27 -0300

    Correção Flip

--- tirarFotoMuitoFoda.py
+++ tirarFotoMuitoFoda.py
@@ -1,22 +1,25 @@
+from time import sleep
+
 import cv2
 from djitellopy import Tello
-from time import sleep
 
 me = Tello()
 me.connect()
 
 print("Bateria: ", me.get_battery())
+me.streamon()
 
 me.takeoff()
-me.move_up(50)
-me.move_forward(70)
-me.flip_back()
+sleep(3)
+me.move_up(200)
+me.move_forward(400)
+me.rotate_counter_clockwise(180)
 
 #tirar foto
-#me.streamon()
-#frame_read = me.get_frame_read()
-#cv2.imwrite("picture.png", frame_read.frame)
-#sleep(10)
+frame_read = me.get_frame_read()
+cv2.imwrite("picture.png", frame_read.frame)
 
-me.move_forward(70)
+me.move_forward(390)
+me.rotate_counter_clockwise(180)
+me.move_down(200)
 me.land()
